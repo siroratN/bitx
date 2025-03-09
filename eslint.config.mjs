c464e1b60bb7f80a13998fb9a4c97e7297bd5177
siroratN <siroratnbs@gmail.com>
2025-03-09 19:10:59 +0700

    ,

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+// const eslintConfig = [
+//   ...compat.extends("next/core-web-vitals", "next/typescript"),
+//   {
+//     rules: {
+//       "no-console": "off",  // อนุญาตให้ใช้ console.log ได้
+//       "react/prop-types": "off", // ปิดการเตือนเรื่อง prop-types ใน React
+//       "@next/next/no-img-element": "off", // อนุญาตให้ใช้ <img> แทน <Image> ของ Next.js
+//     },
+//   },
+// ];
 
-export default eslintConfig;
+// export default eslintConfig;
+export default [];
